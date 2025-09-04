a92a34b06fedb0f276dc3cd8505b7b7ce621554f
Fernando-TB <f.torres17@ufromail.cl>
2025-09-04 01:08:01 -0400

    hola mundo

--- .idea/HolaMundo.java
+++ .idea/HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo
 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo, aqui Fernando Torres");
+        System.out.println("Hola mundo");
+        System.out.println("Fernando Torres");
     }
 }
