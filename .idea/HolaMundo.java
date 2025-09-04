5f5b35c386b686ea58495cc5c810a4424bec5832
Juakamayo <j.pizarro02@ufromail.cl>
2025-09-04 01:14:27 -0400

    Revert "."
    This reverts commit 49ba4805062cd62672199fec136182b8284be02b.

--- .idea/HolaMundo.java
+++ .idea/HolaMundo.java
@@ -2,7 +2,6 @@ public class HolaMundo{
 
 
     public static void main(String[] args) {
-        System.out.print("Hola Mundo ");
-        System.out.print("Joaquin Pizarro");
+        System.out.println("Hola Mundo");
     }
 }
